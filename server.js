5879e7111782c2c80ec7d24a19db1437a98cc680
amarh-ayman <amarhbishtawi@gmail.com>
2021-05-04 22:36:06 +0300

    lab12

--- server.js
+++ server.js
@@ -2,41 +2,39 @@
 require('dotenv').config();
 const express = require('express');
 const server = express();
+const cors = require('cors');
 const superagent = require('superagent');
-
+const pg = require('pg');
 server.set('view engine', 'ejs');
 server.use(express.static('./public'));
 server.use(express.static('./img'));
 
 server.use(express.urlencoded({ extended: true }));
-
+server.use(cors());
 const PORT = process.env.PORT || 3000;
+const client = new pg.Client(process.env.DATABASE_URL);
 
-/*-----------------Start web pages-----------------*/
-server.get('/', homePage);
-server.get('/new', resultPage);
-server.post('/searches', searchPage);
-server.get('*', (req, res) =>
-  res.render('pages/error', { error: 'Page not availabe 404 ERRoR' })
-);
-server.listen(PORT, () => {
-  console.log(`listening on PORT ${PORT}`);
+server.get('/', (req, res) => {
+  let SQL = `SELECT * FROM books`;
+  client
+    .query(SQL)
+    .then(result => {
+      res.render('./pages/index', { booksChosen: result.rows });
+    })
+    .catch(err => {
+      res.render('pages/error', { error: err });
+    });
 });
 
-/*-----------------End web pages-----------------*/
-
-/*--------------start functions Handler----------------*/
-function homePage(req, res) {
-  res.render('pages/index'); ///render for the ejs ,and dont out ext ,the engie will consider what u mean
-}
-function resultPage(req, res) {
+server.get('/searches/new', (req, res) => {
   res.render('pages/searches/new');
-}
+});
 
-function searchPage(req, res) {
+server.post('/searches', (req, res) => {
   let searchbox = req.body.searching;
   let checkbox = req.body.check;
   let bookURL = `https://www.googleapis.com/books/v1/volumes?q=${searchbox}:${checkbox}`;
+
   superagent
     .get(bookURL)
     .then(bookData => {
@@ -46,7 +44,33 @@ function searchPage(req, res) {
     .catch(err => {
       res.render('pages/error', { error: err });
     });
-}
+});
+
+server.post('/books', (req, res) => {
+  let { img, title, author, description, isbn } = req.body;
+  let SQL = `INSERT INTO books (img ,title, author ,description ,isbn) VALUES ($1,$2,$3,$4,$5) RETURNING *;`;
+
+  let safeValues = [img, title, author, description, isbn];
+
+  client
+    .query(SQL, safeValues)
+    .then(result => {
+      res.redirect(`/books/${result.rows[0].id}`);
+    })
+    .catch(err => res.render('pages/error', { error: err }));
+});
+
+server.get('/books/:id', (req, res) => {
+  let SQL = `SELECT * FROM books WHERE id=$1;`;
+  let safeValues = [req.params.id];
+  client
+    .query(SQL, safeValues)
+    .then(result => {
+      res.render('./pages/books/show', { bookViewDetailes: result.rows[0] });
+    })
+    .catch(err => res.render('pages/error', { error: err }));
+});
+
 /*--------------End functions Handler----------------*/
 
 /*------------------render books for show page----------------*/
@@ -55,7 +79,27 @@ function BOOKS(item) {
     item.volumeInfo.imageLinks.smallThumbnail ||
     item.volumeInfo.imageLinks.thumbnail ||
     `https://i.imgur.com/J5LVHEL.jpg`;
+
   this.title = item.volumeInfo.title || 'not avaliable';
   this.author = item.volumeInfo.authors || 'not avaliable';
   this.description = item.volumeInfo.description || 'not avaliable';
+  this.isbn = `${item.volumeInfo.industryIdentifiers[0].type || 'ISBN'}  : ${
+    item.volumeInfo.industryIdentifiers[0].identifier || 'not avaliable'
+  }`;
+
+  // console.log(
+  //   item.volumeInfo.industryIdentifiers[0].identifier || 'not avaliable'
+  // );
+  // console.log(this.isbn);
 }
+
+/************the most dangerous command , so it must be in the bottom******************** */
+client.connect().then(() => {
+  server.listen(PORT, () => {
+    console.log(`listening to PORT ${PORT}`);
+  });
+});
+
+server.get('*', (req, res) =>
+  res.render('pages/error', { error: 'Page not availabe 404 ERRoR' })
+);
